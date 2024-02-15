ad71fbbff9b0c2f1994ce487f2ab11105f053082
RoberMM <rober.sinatra@hotmail.com>
2024-02-14 23:49:54 -0300

    Mais algumas paginas

--- resources/views/site/layouts/_partials/topo.blade.php
+++ resources/views/site/layouts/_partials/topo.blade.php
@@ -1,3 +1,126 @@
+<!-- Topbar Start -->
+<div class="container-fluid">
+    <div class="row align-items-center py-3 px-xl-5">
+        <div class="col-lg-3 d-none d-lg-block">
+            <a href="" class="text-decoration-none">
+                <h1 class="m-0 display-5 font-weight-semi-bold">Vitrine Da Si</h1>
+            </a>
+        </div>
+        <div class="col-lg-6 col-6 text-left">
+            <form action="">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Procure por produtos">
+                    <div class="input-group-append">
+                        <span class="input-group-text bg-transparent text-primary">
+                            <i class="fa fa-search"></i>
+                        </span>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="col-lg-3 col-6 text-right">
+            <a href="" class="btn border">
+                <i class="fas fa-heart text-primary"></i>
+                <span class="badge">0</span>
+            </a>
+            <a href="" class="btn border">
+                <i class="fas fa-shopping-cart text-primary"></i>
+                <span class="badge">0</span>
+            </a>
+        </div>
+    </div>
+</div>
+<!-- Topbar End -->
+
+<!-- Navbar Start -->
+<div class="container-fluid mb-5">
+    <div class="row border-top px-xl-5">
+        <div class="col-lg-3 d-none d-lg-block">
+            <a class="btn shadow-none d-flex align-items-center justify-content-between bg-primary text-white w-100" data-toggle="collapse" href="#navbar-vertical" style="height: 65px; margin-top: -1px; padding: 0 30px;">
+                <h6 class="m-0">Categorias</h6>
+                <i class="fa fa-angle-down text-dark"></i>
+            </a>
+            <nav class="collapse show navbar navbar-vertical navbar-light align-items-start p-0 border border-top-0 border-bottom-0" id="navbar-vertical">
+                <div class="navbar-nav w-100 overflow-hidden" style="height: 410px">
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link" data-toggle="dropdown">Roupas <i class="fa fa-angle-down float-right mt-1"></i></a>
+                        <div class="dropdown-menu position-absolute bg-secondary border-0 rounded-0 w-100 m-0">
+                            <a href="" class="dropdown-item">Masculinas</a>
+                            <a href="" class="dropdown-item">Femininas</a>
+                            <a href="" class="dropdown-item">Crianças</a>
+                        </div>
+                    </div>
+                    <a href="" class="nav-item nav-link">Camisetas</a>
+                    <a href="" class="nav-item nav-link">Jeans</a>
+                    <a href="" class="nav-item nav-link">Praia</a>
+                    <a href="" class="nav-item nav-link">Pijamas</a>
+                    <a href="" class="nav-item nav-link">Esportivas</a>
+                    <a href="" class="nav-item nav-link">Macacões</a>
+                    <a href="" class="nav-item nav-link">Blazers</a>
+                    <a href="" class="nav-item nav-link">Jaquetas</a>
+                    <a href="" class="nav-item nav-link">Calçados</a>
+                </div>
+            </nav>
+        </div>
+        <div class="col-lg-9">
+            <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-0">
+                <a href="" class="text-decoration-none d-block d-lg-none">
+                    <h1 class="m-0 display-5 font-weight-semi-bold"><span class="text-primary font-weight-bold border px-3 mr-1"></span>VitrineDaSi</h1>
+                </a>
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                    <div class="navbar-nav mr-auto py-0">
+                        <a href="index.html" class="nav-item nav-link active">Pagina inicial</a>
+                        <a href="shop.html" class="nav-item nav-link">Catalago</a>
+                        <a href="detail.html" class="nav-item nav-link">Favoritos</a>
+                        <a href="contact.html" class="nav-item nav-link">Contato</a>
+                    </div>
+                    <div class="navbar-nav ml-auto py-0">
+                        <a href="/login" class="nav-item nav-link">Login</a>
+                        <a href="/register" class="nav-item nav-link">Registrar-se</a>
+                    </div>
+                </div>
+            </nav>
+            <div id="header-carousel" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active" style="height: 410px;">
+                        <img class="img-fluid" src="{{ asset('public/img/carousel-1.jpg') }}" alt="Image">
+                        <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                            <div class="p-3" style="max-width: 700px;">
+                                <h4 class="text-light text-uppercase font-weight-medium mb-3">10% Off Na sua primeira compra</h4>
+                                <h3 class="display-4 text-white font-weight-semi-bold mb-4">Vestidos Elegante</h3>
+                                <a href="" class="btn btn-light py-2 px-3">Compre agora</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item" style="height: 410px;">
+                        <img class="img-fluid" src="{{ asset('public/img/carousel-2.jpg') }}" alt="Image">
+                        <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                            <div class="p-3" style="max-width: 700px;">
+                                <h4 class="text-light text-uppercase font-weight-medium mb-3">10% Off Na sua primeira compra</h4>
+                                <h3 class="display-4 text-white font-weight-semi-bold mb-4">Preço Razoável</h3>
+                                <a href="" class="btn btn-light py-2 px-3">Compre agora</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#header-carousel" data-slide="prev">
+                    <div class="btn btn-dark" style="width: 45px; height: 45px;">
+                        <span class="carousel-control-prev-icon mb-n2"></span>
+                    </div>
+                </a>
+                <a class="carousel-control-next" href="#header-carousel" data-slide="next">
+                    <div class="btn btn-dark" style="width: 45px; height: 45px;">
+                        <span class="carousel-control-next-icon mb-n2"></span>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Navbar End -->
 <div class="topo">
     <div class="logo">
         <img src="{{ asset('img/logo.png') }}">
--- resources/views/site/shop.blade.php
+++ resources/views/site/shop.blade.php
@@ -1,6 +1,6 @@
 @extends('site.layouts.basico')
 
-@section('titulo', 'Home')
+@section('titulo', 'Shop')
 
 @section('conteudo')
 
